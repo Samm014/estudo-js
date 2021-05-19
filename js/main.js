51ebf092a94b1c8e96574f5349b20ad6abd0e225
Samuel Medeiros de Albuquerque <84237653+Samm014@users.noreply.github.com>
2021-05-19 06:47:49 -0300

    Envio inicial dos arquivos

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+var d = new Date();
+console.log(d.getMonth()+1) //sempre +1 pois considera o 0
+console.log(d.getDay()) 
+console.log(d.getMinutes()) 
+console.log(d.getSeconds()) // ... 
+
+
+/*var count;
+for(count = 0;count <= 5; count++){
+    console.log(count)
+}
+
+var count = 0;
+
+while(count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+
+
+/*var idade = prompt("Qual é a sua idade?")
+
+//var idade = 18;
+if(idade >= 18){
+    alert('Maioridade')
+}else{
+    alert('Menoridade')
+}
+
+*/
+
+
+
+
+
+/*
+var frutas = [{nome:'maçã',cor:'vermelho'}, {nome:'banana',cor:'amarelo'}]
+console.log(frutas[1].cor)*/
+
+/*var fruta = {nome:'maçã',cor:'vermelha'};
+console.log(fruta.cor)*/
+
+
+
+
+
+/*var nome = "Samuel"
+var msg = "Seja bem vindo "
+
+alert(msg + nome)*/
+
+//var lista = ['maçã','pêra','laranja'];
+//console.log(lista.toString())//retorna em uma string
+//console.log(lista.join(" | "));//substitui as vírgulas na separação dos //objetos
+//lista.push("uva"); //coloca
+//lista.pop("uva"); //retira
+//console.log(lista.length) //quantidade de objetos
+//console.log(lista.reverse()) //altera a ordem
+//console.log(lista)
